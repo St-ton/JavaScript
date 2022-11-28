7cf21da64b4c60fe85c7a76ea15eeb684d42b410
Stanislav Tonkich <st.tonkikh@gmail.com>
2022-11-28 18:22:59 +0100

    2811

--- HomeWork/hw2611/2611.js
+++ HomeWork/hw2611/2611.js
@@ -3,7 +3,7 @@
 // В нижней части страницы должен быть расчет общей суммы.
 // Результат должен выглядеть приблизительно как на макете https://www.figma.com/file/wRonUTYoOVL3ii4meCtdzR/Untitled?node-id=1%3A2&t=5i8ANTCkEpFzMpoi-0
 
-const formProduct = document.querySelector('.form');
+const formProduct = document.querySelector('.form-product');
 const sumPriceContainer = document.querySelector('.sum-price');
 
 const products = [];
@@ -32,6 +32,20 @@ const cardsProductContainer = () => {
   });
 };
 
+const cardRerender = () => {
+  sumPriceContainer.innerText = '';
+
+  const sumPrice = products.reduce((prev1, { price, count }) => prev1 + price * count, 0);
+  const sumCount = products.reduce((prev2, { count }) => prev2 + +count, 0);
+
+  const sumPriceElem = document.createElement('p');
+  const sumCountElem = document.createElement('p');
+
+  sumPriceElem.innerText = `Oбщая стоимость: ${sumPrice}`;
+  sumCountElem.innerText = `Общее количество: ${sumCount}`;
+  sumPriceContainer.append(sumPriceElem, sumCountElem);
+};
+
 formProduct.addEventListener('submit', event => {
   event.preventDefault();
   const { title, price, count } = event.target;
@@ -46,20 +60,9 @@ formProduct.addEventListener('submit', event => {
   price.value = '';
   count.value = '';
 
+  // тут необходимо сделать, чтобы курсор в форме переходил на первое поле
+  // т.е. типа products.title = active
+
   cardsProductContainer();
   cardRerender();
 });
-
-const cardRerender = () => {
-  sumPriceContainer.innerText = '';
-
-  const sumPrice = products.reduce((prev1, { price, count }) => prev1 + price * count, 0);
-  const sumCount = products.reduce((prev2, { count }) => prev2 + count, 0);
-
-  const sumPriceElem = document.createElement('p');
-  const sumCountElem = document.createElement('p');
-
-  sumPriceElem.innerText = `Oбщая стоимость: ${sumPrice}`;
-  sumCountElem.innerText = `Общее количество: ${sumCount}`;
-  sumPriceContainer.append(sumPriceElem, sumCountElem);
-};
